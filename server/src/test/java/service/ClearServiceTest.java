4714bfa71a2d277b1b9629890c6fb3bb28b9e834
Hongting Wang <hongting@byu.edu>
2024-10-22 17:49:04 -0600

    service tests folder refactored to the specified file location in the server directory structure

--- server/src/test/java/service/ClearServiceTest.java
+++ server/src/test/java/service/ClearServiceTest.java
@@ -1,4 +1,4 @@
-package passoff.service;
+package service;
 
 import service.ClearService;
 import dataaccess.DataAccess;
--- server/src/test/java/service/LogoutServiceTest.java
+++ server/src/test/java/service/LogoutServiceTest.java
@@ -1,4 +1,4 @@
-package passoff.service;
+package service;
 
 import service.LogoutService;
 import dataaccess.DataAccess;
