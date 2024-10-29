5fc203f94f97c884b94d580bd30268608f439e51
Hongting Wang <hongting@byu.edu>
2024-10-29 01:16:18 -0600

    MySQLAuthDAO.java implemented

---
+++ server/src/main/java/dataaccess/MySQLAuthDAO.java
@@ -0,0 +1,77 @@
+package dataaccess;
+
+
+import model.AuthData;
+import model.UserData;
+
+
+import java.sql.SQLException;
+
+
+public class MySQLAuthDAO implements AuthDAO {
+    @Override
+    public void createAuth(AuthData authData) throws DataAccessException {
+        var statement = "INSERT INTO auth (username, authToken) VALUES(?, ?";
+
+
+        try (var conn = DatabaseManager.getConnection();
+             var preparedStatement = conn.prepareStatement(statement)) {
+            preparedStatement.setString(1, authData.username());
+            preparedStatement.setString(2, authData.authToken());
+
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error creating auth: " + ex.getMessage());
+        }
+    }
+
+
+    @Override
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        var statement = "SELECT username FROM auth WHERE authToken = ?";
+
+
+        try (var conn = DatabaseManager.getConnection();
+             var preparedStatement  = conn.prepareStatement(statement)) {
+            preparedStatement.setString(1, authToken);
+
+            try (var rs = preparedStatement.executeQuery()) {
+                if (rs.next()) {
+                    String username = rs.getString("username");
+                    return new AuthData(username, authToken);
+                } else {
+                    return null;
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error getting auth: " + ex.getMessage());
+        }
+    }
+
+    @Override
+    public void deleteAuth(String authToken) throws DataAccessException {
+        var statement = "DELETE FROM auth WHERE authToken = ?";
+
+        try (var conn = DatabaseManager.getConnection();
+             var preparedStatement = conn.prepareStatement(statement)) {
+            preparedStatement.setString(1, authToken);
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error deleting auth: " + ex.getMessage());
+        }
+    }
+
+    @Override
+    public void clear() throws DataAccessException {
+        var statement = "DELETE FROM auth";
+
+        try (var conn = DatabaseManager.getConnection();
+             var preparedStatement = conn.prepareStatement(statement)) {
+            preparedStatement.executeUpdate();
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error clearing auth table: " + ex.getMessage());
+        }
+    }
+}
--- server/src/test/java/dataaccess/MySQLUserDAOTest.java
+++ server/src/test/java/dataaccess/MySQLUserDAOTest.java
@@ -1,18 +1,13 @@
 package dataaccess;
 
-
 import model.UserData;
-
 
 import org.junit.jupiter.api.*;
 
-
 import static org.junit.jupiter.api.Assertions.*;
-
 
 class MySQLUserDAOTest {
     private MySQLUserDAO mySQLUserDAO;
-
 
     @BeforeEach
     void setUp() throws DataAccessException {
@@ -20,13 +15,11 @@ class MySQLUserDAOTest {
         mySQLUserDAO.clear();
     }
 
-
     @Test
     @DisplayName("Positive test case for createUser")
     void createUserSuccess() throws DataAccessException {
         UserData testUser = new UserData("testUser", "password123", "testUser@email.com");
         mySQLUserDAO.createUser(testUser);
-
 
         UserData retrievedTestUser = mySQLUserDAO.getUser("testUser");
         assertNotNull(retrievedTestUser, "Retrieved test user should not be null.");
@@ -35,29 +28,24 @@ class MySQLUserDAOTest {
         assertEquals("testUser@email.com", retrievedTestUser.email(), "Email should match.");
     }
 
-
     @Test
     @DisplayName("Negative test case for createUser - Username already exist")
     void createUserFailureAlreadyTaken() throws DataAccessException {
         UserData duplicateUser = new UserData("duplicateUser", "password456", "duplicateUser@email.com");
         mySQLUserDAO.createUser(duplicateUser);
 
-
         DataAccessException ex = assertThrows(DataAccessException.class, () -> {
             mySQLUserDAO.createUser(duplicateUser);
         });
 
-
         assertTrue(ex.getMessage().contains("Error creating user"), "Duplicate user creation should fail.");
     }
-
 
     @Test
     @DisplayName("Positive test case for getUser")
     void getUserSuccess() throws DataAccessException {
         UserData existingUser = new UserData("existingUser", "password789", "existingUser@email.com");
         mySQLUserDAO.createUser(existingUser);
-
 
         UserData retrievedExistingUser = mySQLUserDAO.getUser("existingUser");
         assertNotNull(retrievedExistingUser, "Retrieved existing user should not be null.");
@@ -66,7 +54,6 @@ class MySQLUserDAOTest {
         assertEquals("existingUser@email.com", retrievedExistingUser.email());
     }
 
-
     @Test
     @DisplayName("Negative test case for getUser - User not found")
     void getUserFailureNotFound() throws DataAccessException {
@@ -74,16 +61,13 @@ class MySQLUserDAOTest {
         assertNull(retrievedNonExistingUser, "Retrieved non existing user should be null.");
     }
 
-
     @Test
     @DisplayName("Positive test case for clear")
     void clearSuccess() throws DataAccessException {
         UserData testUser = new UserData("testUser", "password123", "testUser@email.com");
         mySQLUserDAO.createUser(testUser);
 
-
         mySQLUserDAO.clear();
-
 
         UserData retrievedTestUser = mySQLUserDAO.getUser("testUser");
         assertNull(retrievedTestUser, "User table should be cleared.");
